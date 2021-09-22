1d074fd31b91021eb2c06e86ef986b667ea62e63
Soren Abedi <soren.abedi@gmail.com>
2021-09-22 15:48:03 +0330

    feat(alpha): :sparkles: bootstrap `Button` component
    - create associated Types
     - create svg component for handeling svg as input string
     - create button component `StoryBook`
    
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

---
+++ src/lib/components/button/Button.stories.svelte
@@ -0,0 +1,86 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+</script>
+
+<Meta
+	title="Components/Button"
+	component={Button}
+	argTypes={{
+		slot: { control: 'text', defaultValue: 'custom button' },
+		SVGIcon: { control: 'text' },
+		color: {
+			control: {
+				type: 'select',
+				options: ['primary', 'secondary', 'warning', 'danger', 'success', 'default', 'info']
+			},
+			defaultValue: 'default'
+		},
+		variant: {
+			control: { type: 'select', options: ['fill', 'outline', 'outline-gradient'] }
+		},
+		rtl: {
+			control: { type: 'boolean' },
+			defaultValue: false
+		},
+		circle: {
+			control: { type: 'boolean' },
+			defaultValue: false
+		},
+		fullWidth: {
+			control: { type: 'boolean' },
+			defaultValue: false
+		},
+		onClick: { action: 'clicked' }
+	}}
+/>
+
+<Template let:args>
+	<Button variant={'fill'} {...args} on:click={args.onClick}>{args.slot}</Button>
+	<Button variant={'outline'} {...args} on:click={args.onClick}>{args.slot}</Button>
+	<Button variant={'outline-gradient'} {...args} on:click={args.onClick}>{args.slot}</Button>
+</Template>
+
+<Story
+	name="Default"
+	args={{
+		color: 'default'
+	}}
+/>
+<Story
+	name="Primary"
+	args={{
+		color: 'primary'
+	}}
+/>
+<Story
+	name="Secondary"
+	args={{
+		color: 'secondary'
+	}}
+/>
+
+<Story
+	name="Warning"
+	args={{
+		color: 'warning'
+	}}
+/>
+<Story
+	name="Danger"
+	args={{
+		color: 'danger'
+	}}
+/>
+<Story
+	name="Success"
+	args={{
+		color: 'success'
+	}}
+/>
+<Story
+	name="Info"
+	args={{
+		color: 'info'
+	}}
+/>
---
+++ src/lib/components/button/Button.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	import type { OnClickFunc } from '$types/global';
+	import type { ColorProp, VariantProp } from '$types/components/props';
+	import clsx from 'clsx';
+	import { createEventDispatcher } from 'svelte';
+	import Icon from '$lib/utilities/svg-icon.svelte';
+
+	export let rtl = false;
+	export let circle = false;
+	export let SVGIcon = undefined;
+	export let fullWidth = false;
+	export let color: ColorProp = 'default';
+	export let variant: VariantProp = 'outline';
+
+	const dispatch = createEventDispatcher();
+	const onClick: OnClickFunc = (event) => {
+		dispatch('click', event);
+	};
+</script>
+
+<button
+	class={clsx(color, rtl && 'rtl', variant, fullWidth && 'full-width', circle && 'circle')}
+	on:click={onClick}
+>
+	{#if SVGIcon}
+		<span class="icon"><Icon data={SVGIcon} /></span>
+		<span><slot /></span>
+	{:else}
+		<span>
+			<slot />
+		</span>
+	{/if}
+</button>
+
+<style lang="scss">
+	button {
+		@import 'modules/components/button';
+		@include button-outline;
+		@include button-fill;
+		@include button-gradient;
+	}
+</style>
