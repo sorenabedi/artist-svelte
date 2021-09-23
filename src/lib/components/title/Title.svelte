030c3be6ba9d5ab1e3096582c570f39094221800
Soren Abedi <soren.abedi@gmail.com>
2021-09-23 17:38:25 +0330

    feat(alpha): :sparkles: add `Title` component
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

---
+++ src/lib/components/title/Title.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import type { ColorProp, VariantProp } from '$types/components/props';
+	import clsx from 'clsx';
+
+	export let color: ColorProp = 'default';
+	export let variant: VariantProp = 'outline';
+</script>
+
+<h4 class={clsx(color, variant, 'title')}><slot /></h4>
+
+<style lang="scss">
+	.title {
+		@import 'modules/components/title';
+	}
+</style>
