65f74a1a4aeeccbb3e602d9bba39e63fa68cfdb0
Soren Abedi <soren.abedi@gmail.com>
2021-10-03 19:47:47 +0330

    style(dev): :art: format codes `pnpm format`
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/tests/components/input.spec.jsx
+++ src/tests/components/input.spec.jsx
@@ -3,31 +3,31 @@ import { render } from '@testing-library/svelte';
 
 describe('Input component test suite', () => {
 	it('it works', async () => {
-		const component = render(Input,{props:{ id: 'input-id-1' }});
+		const component = render(Input, { props: { id: 'input-id-1' } });
 		expect(() => component.getAllByTestId('Input')).not.toThrow();
 	});
 	it('testing default classes', async () => {
-		const { getByTestId } = render(Input,{props:{ id: 'input-id-1' }});
+		const { getByTestId } = render(Input, { props: { id: 'input-id-1' } });
 		expect(getByTestId('Input')).toHaveClass('default', 'input');
 	});
 	it('testing a11y label HTMLFor', async () => {
-		const { getByTestId ,component} = render(Input);
+		const { getByTestId, component } = render(Input);
 		expect(getByTestId('Input').querySelector('input')).not.toHaveAttribute('id');
 		expect(getByTestId('Input').querySelector('label')).not.toHaveAttribute('for');
 		component.$$set({ id: 'input-id-34' });
 		await tick();
-		expect(getByTestId('Input').querySelector('input')).toHaveAttribute('id','input-id-34');
-		expect(getByTestId('Input').querySelector('label')).toHaveAttribute('for','input-id-34');
+		expect(getByTestId('Input').querySelector('input')).toHaveAttribute('id', 'input-id-34');
+		expect(getByTestId('Input').querySelector('label')).toHaveAttribute('for', 'input-id-34');
 	});
 	it('testing input type change', async () => {
-		const { getByTestId ,component} = render(Input,{props:{ id: 'input-id-1' }});
-		expect(getByTestId('Input').querySelector('input')).toHaveAttribute('type','text');
+		const { getByTestId, component } = render(Input, { props: { id: 'input-id-1' } });
+		expect(getByTestId('Input').querySelector('input')).toHaveAttribute('type', 'text');
 		component.$$set({ type: 'search' });
 		await tick();
-		expect(getByTestId('Input').querySelector('input')).toHaveAttribute('type','search');
+		expect(getByTestId('Input').querySelector('input')).toHaveAttribute('type', 'search');
 	});
 	it('testing interactive color prop change', async () => {
-		const { getByTestId, component } = render(Input,{props:{ id: 'input-id-1' }});
+		const { getByTestId, component } = render(Input, { props: { id: 'input-id-1' } });
 		expect(getByTestId('Input')).toHaveClass('default');
 		expect(getByTestId('Input')).not.toHaveClass('primary');
 		component.$$set({ color: 'primary' });
@@ -38,7 +38,7 @@ describe('Input component test suite', () => {
 		expect(getByTestId('Input')).toHaveClass('danger');
 	});
 	it('testing interactive variant prop change', async () => {
-		const { getByTestId, component } = render(Input,{props:{ id: 'input-id-1' }});
+		const { getByTestId, component } = render(Input, { props: { id: 'input-id-1' } });
 		expect(getByTestId('Input')).toHaveClass('fill');
 		expect(getByTestId('Input')).not.toHaveClass('outline');
 		component.$$set({ variant: 'outline' });
@@ -49,7 +49,7 @@ describe('Input component test suite', () => {
 		expect(getByTestId('Input')).toHaveClass('fill');
 	});
 	it('testing interactive shadow prop change', async () => {
-		const { getByTestId, component } = render(Input,{props:{ id: 'input-id-1' }});
+		const { getByTestId, component } = render(Input, { props: { id: 'input-id-1' } });
 		expect(getByTestId('Input')).not.toHaveClass('shadow');
 		component.$$set({ shadow: true });
 		await tick();
@@ -59,7 +59,7 @@ describe('Input component test suite', () => {
 		expect(getByTestId('Input')).not.toHaveClass('shadow');
 	});
 	it('testing interactive fullWidth prop change', async () => {
-		const { getByTestId, component } = render(Input,{props:{ id: 'input-id-1' }});
+		const { getByTestId, component } = render(Input, { props: { id: 'input-id-1' } });
 		expect(getByTestId('Input')).not.toHaveClass('fullWidth');
 		component.$$set({ fullWidth: true });
 
@@ -70,7 +70,7 @@ describe('Input component test suite', () => {
 		expect(getByTestId('Input')).not.toHaveClass('fullWidth');
 	});
 	it('testing interactive rtl prop change', async () => {
-		const { getByTestId, component } = render(Input,{props:{ id: 'input-id-1' }});
+		const { getByTestId, component } = render(Input, { props: { id: 'input-id-1' } });
 		expect(getByTestId('Input')).not.toHaveClass('rtl');
 		component.$$set({ rtl: true });
 
