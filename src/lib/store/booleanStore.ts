d89803c3c6789197ced99d8b2e865a8342c59ba9
Soren Abedi <soren.abedi@gmail.com>
2021-10-04 20:31:20 +0330

    refactor(dev): :art: run `pnpm format`
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/lib/store/booleanStore.ts
+++ src/lib/store/booleanStore.ts
@@ -1,11 +1,13 @@
 import { writable } from 'svelte/store';
 import type { Writable } from 'svelte/store';
 
-export default (initial: boolean):{
-    isOpen: Writable<boolean>;
-    open: () => void;
-    close: () => void;
-    toggle: () => void;
+export default (
+	initial: boolean
+): {
+	isOpen: Writable<boolean>;
+	open: () => void;
+	close: () => void;
+	toggle: () => void;
 } => {
 	const isOpen = writable<boolean>(initial);
 	const { set, update } = isOpen;
