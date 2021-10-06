85ad7cd22f57f6ae23e0224eaac3bb807fa7c091
Soren Abedi <soren.abedi@gmail.com>
2021-10-06 04:06:49 +0330

    feat(alpha): :sparkles: add svg icon support for `Input` component
    - improve `Input` and `Avatar` tests
    - run `pnpm format`
    
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/lib/components/input/Input.svelte
+++ src/lib/components/input/Input.svelte
@@ -2,11 +2,13 @@
 	import type { ColorProp, InputTypeProp, VariantProp } from '../../types/components';
 	import clsx from 'clsx';
 	import { nanoid } from 'nanoid';
+	import SvgIcon from '../../utilities/svg-icon.svelte';
 	const testID = process.env.NODE_ENV === 'test' ? 'Input' : /* istanbul ignore next */ undefined;
 
 	export let color: ColorProp = 'default';
 	export let variant: Exclude<VariantProp, 'outline-gradient'> = 'fill';
 	export let shadow = false;
+	export let icon: string | undefined = undefined;
 	export let id: string = `i-${nanoid(5)}`;
 	export let type: InputTypeProp = 'text';
 	export let rtl = false;
@@ -18,23 +20,33 @@
 	data-testid={testID}
 	class={clsx(color, variant, 'input')}
 	class:shadow
+	class:icon
 	class:fullWidth
 	class:rtl
 >
-	<input
-		{id}
-		{type}
-		aria-label={label && label.textContent}
-		on:change
-		on:click
-		on:dblclick
-		on:input
-		placeholder=" "
-		{...$$restProps}
-	/>
-	<label for={id} bind:this={label}>
-		<slot />
-	</label>
+	<div class="inputContainer">
+		<input
+			{id}
+			{type}
+			aria-label={label && label.textContent}
+			on:change
+			on:click
+			on:dblclick
+			on:input
+			placeholder=" "
+			{...$$restProps}
+		/>
+		<label for={id} bind:this={label}>
+			{#if icon}
+				<div class="icon">
+					<SvgIcon data={icon} />
+				</div>
+			{/if}
+			<div class="label">
+				<slot />
+			</div>
+		</label>
+	</div>
 </div>
 
 <style lang="scss">
--- src/routes/input.svelte
+++ src/routes/input.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { Input } from '$lib';
+	import close from '$lib/svg/icons/close';
+	import more from '$lib/svg/icons/more';
 </script>
 
 <div dir="rtl">
@@ -18,7 +20,7 @@
 	<Input shadow color="secondary" type="text">some text</Input>
 	<Input shadow color="success" type="text">some text</Input>
 	<Input shadow color="warning" type="text">some text</Input>
-	<Input shadow color="danger" type="text">some text</Input>
+	<Input shadow color="danger" icon={close} type="text">some text</Input>
 	<Input shadow color="info" type="text">some text</Input>
 </div>
 
@@ -27,7 +29,9 @@
 	<Input variant="outline" rtl color="primary" type="text">ایمیل خود را وارد کنید</Input>
 	<Input variant="outline" rtl color="secondary" type="text">ایمیل خود را وارد کنید</Input>
 	<Input variant="outline" rtl color="success" type="text">ایمیل خود را وارد کنید</Input>
-	<Input variant="outline" rtl color="warning" type="text">ایمیل خود را وارد کنید</Input>
+	<Input variant="outline" rtl color="warning" icon={more} shadow type="text"
+		>ایمیل خود را وارد کنید</Input
+	>
 	<Input variant="outline" rtl color="danger" type="text">ایمیل خود را وارد کنید</Input>
 	<Input variant="outline" rtl color="info" type="text">ایمیل خود را وارد کنید</Input>
 </div>
