6c0ad76f4bccfe4d584e4e0a8ca304ebb8446a01
Soren Abedi <soren.abedi@gmail.com>
2021-11-05 16:37:42 +0330

    feat(patch): :sparkles: implement `HeadingType` and `muted` properties for `Title` component
    - update associated unit tests
    
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/lib/components/title/Title.svelte
+++ src/lib/components/title/Title.svelte
@@ -6,25 +6,175 @@
 
 	export let color: ColorProp = 'default';
 	export let variant: VariantProp = 'outline';
+	export let muted: boolean = false;
+	export let headingType: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'span' | 'strong' | 'em' | 'i' =
+		'h4';
 	export let useAction: useAction = () => ({});
 	let className: string | undefined = undefined;
 	export { className as class };
 </script>
 
-<h4
-	class={clsx(color, variant, 'title', className)}
-	use:useAction
-	data-testid={testID}
-	on:click
-	on:dblclick
-	on:mouseenter
-	on:mouseleave
-	on:mouseover
-	on:focus
-	{...$$restProps}
->
-	<slot />
-</h4>
+{#if headingType === 'h1'}
+	<h1
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</h1>
+{:else if headingType === 'h2'}
+	<h2
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</h2>
+{:else if headingType === 'h3'}
+	<h3
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</h3>
+{:else if headingType === 'h4'}
+	<h4
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</h4>
+{:else if headingType === 'h5'}
+	<h5
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</h5>
+{:else if headingType === 'h6'}
+	<h6
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</h6>
+{:else if headingType === 'span'}
+	<span
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</span>
+{:else if headingType === 'strong'}
+	<strong
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</strong>
+{:else if headingType === 'em'}
+	<em
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</em>
+{:else if headingType === 'i'}
+	<i
+		class={clsx(color, variant, 'title', className)}
+		class:muted
+		use:useAction
+		data-testid={testID}
+		on:click
+		on:dblclick
+		on:mouseenter
+		on:mouseleave
+		on:mouseover
+		on:focus
+		{...$$restProps}
+	>
+		<slot />
+	</i>
+{/if}
 
 <style lang="scss">
 	.title {
