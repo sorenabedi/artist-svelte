0b347bc040eac47493cdbe30b5d0c82740f1ff37
Soren Abedi <soren.abedi@gmail.com>
2021-11-08 19:32:02 +0330

    fix: :rotating_light: compiler error when using webpack
    - removing check for `import.meta.env` availablity in `ArtistEnv`
    - update `package.json` dependencies
    - update `README.md`
    
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

--- src/lib/env.ts
+++ src/lib/env.ts
@@ -1,7 +1,5 @@
 import Booleanify from './utilities/helper/booleanify';
 
 export default {
-	RTL: Booleanify(
-		typeof process !== 'undefined' ? process.env['VITE_RTL'] : import.meta.env.VITE_RTL
-	)
+	RTL: Booleanify(process.env.VITE_RTL)
 };
