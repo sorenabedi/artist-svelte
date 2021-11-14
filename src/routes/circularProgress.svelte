b144d146ef6cc5f5b6e90fa6cbaea01775c27870
Soren Abedi <soren.abedi@gmail.com>
2021-11-14 23:54:10 +0330

    feat(alpha): :sparkles: bootstrap `circularProgress` component
    - add associated `Storybook`
    - add unit tests
    - add examples route page
    - update `useAction` type to support custom elements
    
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

---
+++ src/routes/circularProgress.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	import Gauge from '$lib/components/circularProgress';
+
+	const RandomPercentage = () => {
+		return Math.random() * 100;
+	};
+</script>
+
+<div>
+	<Gauge size="6rem" color="primary" />
+	<Gauge size="6rem" color="secondary" />
+	<Gauge size="6rem" color="danger" on:change={(e) => console.log(e.target)} />
+	<Gauge size="6rem" color="default" />
+	<Gauge size="6rem" color="info" />
+	<Gauge size="6rem" color="success" />
+	<Gauge size="6rem" color="warning" />
+</div>
+<div>
+	<Gauge size="6rem" color="primary" percentage={RandomPercentage()} />
+	<Gauge size="6rem" color="secondary" percentage={RandomPercentage()} />
+	<Gauge size="6rem" color="danger" percentage={RandomPercentage()} />
+	<Gauge size="6rem" color="default" percentage={RandomPercentage()} />
+	<Gauge size="6rem" color="info" percentage={RandomPercentage()} />
+	<Gauge size="6rem" color="success" percentage={RandomPercentage()} />
+	<Gauge size="6rem" color="warning" percentage={RandomPercentage()} />
+</div>
+<div>
+	<Gauge size="6rem" color="primary" percentage={RandomPercentage()} showPercentage />
+	<Gauge size="6rem" color="secondary" percentage={RandomPercentage()} showPercentage />
+	<Gauge size="6rem" color="danger" percentage={RandomPercentage()} showPercentage />
+	<Gauge size="6rem" color="default" percentage={RandomPercentage()} showPercentage />
+	<Gauge size="6rem" color="info" percentage={RandomPercentage()} showPercentage />
+	<Gauge size="6rem" color="success" percentage={RandomPercentage()} showPercentage />
+	<Gauge size="6rem" color="warning" percentage={RandomPercentage()} showPercentage />
+</div>
+<div>
+	<Gauge size="6rem" color="primary" percentage={RandomPercentage()} noSpin showPercentage />
+	<Gauge size="6rem" color="secondary" percentage={RandomPercentage()} noSpin showPercentage />
+	<Gauge size="6rem" color="danger" percentage={RandomPercentage()} noSpin showPercentage />
+	<Gauge size="6rem" color="default" percentage={RandomPercentage()} noSpin showPercentage />
+	<Gauge size="6rem" color="info" percentage={RandomPercentage()} noSpin showPercentage />
+	<Gauge size="6rem" color="success" percentage={RandomPercentage()} noSpin showPercentage />
+	<Gauge size="6rem" color="warning" percentage={RandomPercentage()} noSpin showPercentage />
+</div>
+
+<style>
+	div {
+		display: block;
+	}
+</style>
---
+++ src/stories/components/CircularProgress.stories.svelte
@@ -0,0 +1,91 @@
+<script>
+	import { Meta, Template, Story } from '@storybook/addon-svelte-csf';
+	import CircularProgress from '$lib/components/circularProgress';
+</script>
+
+<Meta
+	title="Components/CircularProgress"
+	component={CircularProgress}
+	argTypes={{
+		color: {
+			control: {
+				type: 'select',
+				options: ['primary', 'secondary', 'warning', 'danger', 'success', 'default', 'info']
+			},
+			defaultValue: 'default'
+		},
+		size: { control: 'text', defaultValue: '6em' },
+		percentage: {
+			control: { type: 'number' }
+		},
+		showPercentage: {
+			control: { type: 'boolean' },
+			defaultValue: false
+		},
+		noSpin: {
+			control: { type: 'boolean' },
+			defaultValue: false
+		},
+		desimalPoint: {
+			control: { type: 'number' }
+		},
+		onClick: { action: 'clicked' },
+		onChange: { action: 'changed' },
+		onDblClick: { action: 'dblclicked' },
+		onInput: { action: 'inputed' }
+	}}
+/>
+
+<Template let:args>
+	<CircularProgress
+		{...args}
+		on:click={args.onClick}
+		on:change={args.onChange}
+		on:dblclick={args.onDblClick}
+		on:input={args.onInput}
+	/>
+</Template>
+
+<Story
+	name="Default"
+	args={{
+		color: 'default'
+	}}
+/>
+<Story
+	name="Primary"
+	args={{
+		color: 'primary'
+	}}
+/>
+<Story
+	name="Secondary"
+	args={{
+		color: 'secondary'
+	}}
+/>
+
+<Story
+	name="Warning"
+	args={{
+		color: 'warning'
+	}}
+/>
+<Story
+	name="Danger"
+	args={{
+		color: 'danger'
+	}}
+/>
+<Story
+	name="Success"
+	args={{
+		color: 'success'
+	}}
+/>
+<Story
+	name="Info"
+	args={{
+		color: 'info'
+	}}
+/>
