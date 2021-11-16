222681f0d350c6d54cdfa09f8d7c0ca36f35bcbb
Soren Abedi <soren.abedi@gmail.com>
2021-11-16 13:49:52 +0330

    feat(docs-only): :camera_flash: add `getting-started` template to project
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

---
+++ examples/getting-started/src/lib/svg/index.ts
@@ -0,0 +1,5 @@
+import SassIcon from './sass-icon';
+import SvelteIcon from './svelte-icon';
+import typeScriptIcon from './typescript-icon';
+
+export { SassIcon, SvelteIcon, typeScriptIcon };
---
+++ examples/getting-started/svelte.config.js
@@ -0,0 +1,23 @@
+import preprocess from 'svelte-preprocess';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: preprocess({
+		sass: {},
+		replace: [
+			[/process\.env\['NODE_ENV'\]/g, () => `import.meta.env.MODE`],
+			[/process\.env\.NODE_ENV/g, () => `import.meta.env.MODE`],
+			[/process\.env\['(\w+)'\]/g, (_, match) => JSON.stringify(process.env[match])],
+			[/process\.env\.(\w+)/g, (_, match) => JSON.stringify(process.env[match])]
+		]
+	}),
+
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
+	}
+};
+
+export default config;
