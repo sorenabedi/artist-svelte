c4a8523fd9a0d36cd7728683be6a5d5694064a69
Soren Abedi <soren.abedi@gmail.com>
2021-11-21 15:51:49 +0330

    chore:(docs-only) :camera_flash: add `getting-started-sass` example
    Signed-off-by: Soren Abedi <soren.abedi@gmail.com>

---
+++ examples/getting-started-sass/src/routes/__layout.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+</script>
+
+<slot />
+
+<style lang="scss" global>
+	/* Artist UI global styles (required) */
+	/* Normalize css styles in SCSS (optional) */
+	@import '../../node_modules/@sorens/artist-svelte/scss/GlobalStyles';
+	@import '../../node_modules/@sorens/artist-svelte/scss/modules/normalize';
+
+	/* Any other global styles that you might need, goes here. e.g: */
+	html,
+	body {
+		margin: 0;
+		padding: 0;
+		font-family: 'Poppins', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
+			Arial, 'Noto Sans', 'Liberation Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji',
+			'Segoe UI Symbol', 'Noto Color Emoji', Vazir;
+	}
+	html {
+		background-color: hsl(var(--bg-color));
+		color: hsl(var(--fg-color));
+		transition: color 0.3s, background-color 0.3s;
+	}
+	::-webkit-scrollbar {
+		width: 5px;
+		height: 5px;
+		display: block;
+		background: hsl(var(--bg-color));
+	}
+	::-webkit-scrollbar-thumb {
+		background: hsl(var(--fg-color));
+		border-radius: 5px;
+	}
+</style>
---
+++ examples/getting-started-sass/src/routes/index.svelte
@@ -0,0 +1,93 @@
+<script lang="ts">
+	import { SvelteIcon, SassIcon, typeScriptIcon } from '$lib/svg';
+	import { TextBlock, Link, Button, SvgParser } from '@sorens/artist-svelte';
+</script>
+
+<section>
+	<TextBlock color="primary" elementType="h1">Artist UI</TextBlock>
+	<TextBlock color="secondary" elementType="p">a clean UI framework for Sveltekit</TextBlock>
+	<div class="primaryButtonContainer">
+		<Button
+			color="primary"
+			variant="fill"
+			target="_blank"
+			rel="noopener"
+			href="https://artist-ui.pages.dev/getting-started/installation"
+		>
+			Getting started
+		</Button>
+		<Button
+			color="primary"
+			target="_blank"
+			rel="noopener"
+			href="https://artist-ui.pages.dev/components"
+		>
+			Components
+		</Button>
+	</div>
+	<div class="techs">
+		<Link
+			color="default"
+			href="https://kit.svelte.dev"
+			target="_blank"
+			rel="noopener"
+			TextBlock="sveltekit website"
+		>
+			<SvgParser data={SvelteIcon} size="3rem" />
+			<TextBlock elementType="strong" color="default" muted uppercase>
+				Created With SvelteKit
+			</TextBlock>
+		</Link>
+		<Link
+			color="default"
+			href="https://www.typescriptlang.org"
+			target="_blank"
+			rel="noopener"
+			TextBlock="typescript website"
+		>
+			<SvgParser data={typeScriptIcon} size="3rem" />
+			<TextBlock elementType="strong" color="default" muted uppercase>TypeScript Support</TextBlock>
+		</Link>
+		<Link
+			color="default"
+			href="https://sass-lang.com"
+			target="_blank"
+			rel="noopener"
+			TextBlock="sass-lang website"
+		>
+			<SvgParser data={SassIcon} size="3rem" />
+			<TextBlock elementType="strong" color="default" muted uppercase>Styled with SCSS</TextBlock>
+		</Link>
+	</div>
+</section>
+
+<style lang="scss">
+	section {
+		text-align: center;
+		margin-block: 3rem;
+		padding: 2rem;
+		line-height: 1.7;
+	}
+
+	.primaryButtonContainer {
+		margin-block-start: 1.5rem;
+		margin-block-end: 3rem;
+	}
+	.techs {
+		text-align: center;
+		width: 100%;
+		:global {
+			> a {
+				margin-block: 1.5rem;
+				margin-inline: 1.5rem;
+				display: flex;
+				justify-content: space-around;
+				align-items: center;
+
+				strong {
+					display: block;
+				}
+			}
+		}
+	}
+</style>
